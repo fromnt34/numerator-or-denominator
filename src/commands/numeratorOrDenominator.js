07947f9e0c5ca5d8323ef6304e29c2dfe5c4208e
Dmitry Kurtsev <dmitrykurtsev34@gmail.com>
2021-11-20 21:16:29 +0300

    Add sunday check

--- src/commands/numeratorOrDenominator.js
+++ src/commands/numeratorOrDenominator.js
@@ -5,6 +5,7 @@ const {
 	getWeekNumber
 } = require('../helpers/dates/dates');
 const { isEven } = require('../helpers/numbers/numbers');
+const { textNewline } = require('../helpers/text/text');
 
 
 
@@ -12,15 +13,37 @@ function numeratorOrDenominator(context) {
 	const numeratorString = 'Числитель';
 	const denominatorString = 'Знаменатель';
 
+	let additionalInformation = '';
+
 
 	const message = context.message;
+
 	const messageSendingTimestamp = message.date;
+
 	const messageSendingDate = timestampToDate(messageSendingTimestamp);
 	const messageSendingLocalDate = convertUTCDateToLocalDate(messageSendingDate);
+
 	const weekNumber = getWeekNumber(messageSendingLocalDate);
 
+	const isWeekEven = isEven(weekNumber);
 
-	context.reply(isEven(weekNumber) ? denominatorString : numeratorString);
+
+	// check if message sending day of week is sunday
+	const messageSendingDayOfWeek = messageSendingLocalDate.getUTCDay();
+
+	if (messageSendingDayOfWeek === 0) {
+		additionalInformation =
+			textNewline.repeat(2) +
+			'Обратите внимание, что сегодня последний день текущей недели (воскресенье), а это значит, что уже завтра (понедельник) неделя будет иметь статус: ' +
+			(isWeekEven ? numeratorString : denominatorString) +
+			'.';
+	}
+
+
+
+	context.reply(
+		(isWeekEven ? denominatorString : numeratorString) + additionalInformation
+	);
 }
 
 
